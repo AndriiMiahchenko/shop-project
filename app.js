733e9e0a4be0f0101edcd81cba3009b014775518
Andrii <miyagkiy12@gmail.com>
2022-07-31 20:11:55 +0300

    add slider

--- app.js
+++ app.js
@@ -6,45 +6,78 @@ console.log(addToCartBtns);
 
 for (let i = 0; i < addToCartBtns.length; i++) {
   addToCartBtns[i].addEventListener("click", function () {
-    console.log("clicked");
+    let prevProductsCount = +productsCountEl.textContent;
+    productsCountEl.textContent = prevProductsCount + 1;
+    // productsCountEl.textContent = +prevProductsCount.textContent + 1;
   });
 }
 
-document.getElementById("like-click1").onclick = function () {
-  this.classList.toggle("like-style");
-};
 
-document.getElementById("like-click2").onclick = function () {
-  this.classList.toggle("like-style");
-};
+let likeBtns = document.querySelectorAll(".like");
+console.log(likeBtns);
 
-document.getElementById("like-click3").onclick = function () {
-  this.classList.toggle("like-style");
+likeBtns.forEach((item) => item.addEventListener("click", function () {
+  if (item.classList.contains("liked")) {
+    item.classList.remove("liked");
+  } else {
+    item.classList.add("liked");
+  }
+  //item.classList.toggle("liked");
+})
+)
+
+//more details
+let moreDetailsBtns = document.querySelectorAll(".btn-more-details");
+let modal = document.querySelector(".modal");
+let btnClose = document.querySelector(".btn-hover");
+
+function openModal() {
+  modal.classList.add("show");
+  modal.classList.remove("hide");
 }
 
-document.getElementById("like-click4").onclick = function () {
-  this.classList.toggle("like-style");
+function closeModal() {
+  modal.classList.add("hide");
+  modal.classList.remove("show");
 }
 
-document.getElementById("like-click5").onclick = function () {
-  this.classList.toggle("like-style");
+moreDetailsBtns.forEach((item) => item.addEventListener("click", openModal));
+btnClose.addEventListener("click", closeModal);
+
+
+$(".slider-block").slick({
+  autoplay: true,
+  dots: true,
+})
+
+const modalItems = document.querySelectorAll('.modal');
+
+if (modalItems.length > 0) {
+  window.addEventListener('scroll', modalOnScroll);
+
+  function modalOnScroll() {
+    for (let i = 0; i < modalItems.length; i++) {
+      const modalItems = modalItems[i];
+      const modalItemsHeight = modalItems.offsetHeight;
+      const modalItemsOffset = offset(modalItems).top;
+      const modalStart = 2;
+
+      let modalItemsPoint = window.innerHeight - modalItemsHeight / modalStart;
+      if (modalItemsHeight > window.innerHeight) {
+        modalItemsPoint = window.innerHeight - window.innerHeight / modalStart;
+      }
+
+      if ((pageYOffset > modalItemsOffset - modalItemsPoint) && (pageYOffset < (modalItemsOffset + modalItemsHeight))) {
+        modalItems.classList.add('show');
+      } else {
+        modalItems.classList.remove('show');
+      }
+    }
+  }
 }
 
-document.getElementById("like-click6").onclick = function () {
-  this.classList.toggle("like-style");
+function offset(el) {
+  const rect = el.getBoundingClientRect(),
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  return { top: rect.top + scrollTop };
 }
-
-document.getElementById("like-click7").onclick = function () {
-  this.classList.toggle("like-style");
-}
-
-document.getElementById("like-click8").onclick = function () {
-  this.classList.toggle("like-style");
-}
-
-document.getElementById("like-click9").onclick = function () {
-  this.classList.toggle("like-style");
-}
-
-
-
